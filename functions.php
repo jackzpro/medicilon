4632c988a8f6d0576955d67f47ce281387338145
jackzpro <jackzhangpc@outlook.com>
2017-02-23 11:41:02 +0800

    first commit

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Medicilon functions and definitions
+ *
+ * @link http://www.medicilon.com
+ *
+ * @package WordPress
+ * @subpackage medicilon
+ * @since 0.1
+ */
+
+/**
+ * medicilon only works in WordPress 4.7 or later.
+ */
